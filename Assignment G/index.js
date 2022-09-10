e25c030d350430fb2b020ce9e442f0349bb83e98
Awosise Oluwaseun <awosiseo@gmail.com>
2022-09-10 19:33:25 +0100

    index.js edit

--- Assignment G/index.js
+++ Assignment G/index.js
@@ -1,11 +1,19 @@
 const http = require('http');
 const fs = require('fs');
 const path = require('path');
-import createBook from './modules/book modules/createBook'
-import deleteBook from './modules/book modules/deleteBook'
-import loanOutBook from './modules/book modules/createBook'
-import returnBook from './modules/book modules/returnBook'
-import updateBook from './modules/book modules/updateBook'
+
+
+const modulePath = path.join(__dirname, '/modules/book modules/')
+const createBook = require(`${modulePath}createBook`);
+const deleteBook = require(`${modulePath}deleteBook`);
+const loanOutBook = require(`${modulePath}loanOutBook`);
+const returnBook = require(`${modulePath}returnBook`);
+const updateBook = require(`${modulePath}updateBook`);
+// import createBook from './modules/book modules/createBook.js';
+// import deleteBook from './modules/book modules/deleteBook.js';
+// import loanOutBook from'./modules/book modules/loanOutBook.js';
+// import returnBook from './modules/book modules/returnBook.js';
+// import updateBook from './modules/book modules/updateBook.js';
 
 
 host = 'localhost';
@@ -17,19 +25,20 @@ function requestHandler(req, res){
     let req_url  = req.url.toLowerCase();
     let req_method = req.method.toLowerCase();
 
-    if (req_url === 'books/create' && req_method === 'post'){
+    if (req_url === '/books/create' && req_method === 'post'){
         createBook(req,res)
+        res.end()
     }
-    if (req_url === 'books/delete' && req_method === 'delete'){
+    else if (req_url === '/books/delete' && req_method === 'delete'){
         deleteBook(req,res)
     }
-    if (req_url === 'books/loanout' && req_method === 'post'){
+    else if (req_url === '/books/loanout' && req_method === 'post'){
         loanOutBook(req,res)
     }
-    if (req_url === 'books/return' && req_method === 'post'){
+    else if (req_url === '/books/return' && req_method === 'post'){
         returnBook(req,res)
     }
-    if (req_url === 'books/update' && req_method === 'put'){
+    else if (req_url === '/books/update' && req_method === 'put'){
         updateBook(req,res)
     }else {
         res.writeHead(404);
